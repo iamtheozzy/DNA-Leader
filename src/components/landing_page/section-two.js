6ab47bb2e0f49006212e0d929c4fae0d65049446
Ozzy Cardenas <ocardenas@apple.com>
2019-02-07 16:53:00 -0800

    finished styling section2

--- src/components/landing_page/section-two.js
+++ src/components/landing_page/section-two.js
@@ -7,19 +7,27 @@ const Text = 'becoming privatley healthy & publicly fruitful leaders'
 
 const Container = styled.div`
     display: flex;
-    background-color: ##f2efe9;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
+    align-content: space-around;
+    background-color: #f2efe9;
     width: 100%;
-    padding: 12rem 0;
+    height: 650px;
+    
 `;
 
 const Left = styled.div`
     display: flex;
     justify-content: center;
-    width: 50%;
+    width: 45%;
+    min-width: 640px;
+    max-width: 650px;
+    height: 250px;
 `;
 
 const Question = styled.p`
-    font-size: 2rem;
+    font-size: 5rem;
     line-height: 100%;
     display: flex;
     align-items: center;
@@ -29,20 +37,43 @@ const LineBreak = styled.br`
 `
 
 const LoremDiv = styled.div`
+    display: flex;
+    align-items: center;
     overflow: hidden;
-    width: 30%;
-    font-size: 1.25em;
+    width: 55%;
+    max-height: 26em;
+    min-width: 370px;;
+    font-size: 2.5em;
     margin-left: 1em;
     border-left: 2px solid #212121;
     padding-left: 1.33em;
 `;
 
 const Right = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 45%;
+    min-width: 640px;
+    max-width: 650px;
+    height: 250px;
 `;
 
+const Slogan = styled.h1`
+    color: #c4ab9f;
+    font-size: 4em;
+    max-width: 355px;
+`;
 
-
+const SubBtn = styled.button`
+    font-size: 2.5em;
+    background: #9e7a20;
+    color: #f2efe9;
+    padding: .7em 4em;
+    text-align: center;
+    max-width: 350px;
+    margin-top: 2em;
+`;
 
 
 
@@ -53,8 +84,14 @@ const SectionTwo = props => (
             <Question>WHY<LineBreak />DNA?</Question>
             <LoremDiv>{Lorem}</LoremDiv>
         </Left>
-        
-    
+        <Right>
+            <Slogan>
+                becoming privately <LineBreak />
+                healthy & publicly <LineBreak />
+                fruitful leaders
+            </Slogan>
+            <SubBtn>SUBSCRIBE</SubBtn>
+        </Right>
     </Container>
 )
 
