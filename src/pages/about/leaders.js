177d6594394689433edf793cabc4a85cac100258
Ozzy Cardenas <ocardenas@apple.com>
2019-02-23 10:48:42 -0800

    deployment bug fix

---
+++ src/pages/about/leaders.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+
+
+const LeadersPage = () => (
+  <Layout>
+    <h1>Hello! This is the Leaders page</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default LeadersPage
